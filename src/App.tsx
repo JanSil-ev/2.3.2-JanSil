0fe94a966089b0db215fe70675ebe1e181eb5e53
JanSil-ev <tjplay0099@gmail.com>
2025-08-27 17:21:21 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import '@mantine/core/styles.css';
-
 import { MantineProvider } from '@mantine/core';
 import { theme } from './theme';
-import { HomePage } from './pages/Home.page';
+import { Main } from './components/Main';
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <HomePage />
+      <Main/>
     </MantineProvider>
   );
 }
